8bdcb70502cf968ca69c36b51924c53153e1891a
Richard Haeser <richard@richardhaeser.com>
2021-03-24 15:08:28 +0100

    [TASK] Make sure TypoScript is always there

--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('TYPO3_MODE') or die();
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('webvitals', 'Configuration/TypoScript', 'Web Vitals');
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,9 @@
 <?php
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][]
     = \RichardHaeser\WebVitals\Hooks\PageRendererPreProcess::class . '->render';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+    'WebVitals',
+    'setup',
+    '<INCLUDE_TYPOSCRIPT: source="FILE: EXT:webvitals/Configuration/TypoScript/setup.typoscript">'
+);
